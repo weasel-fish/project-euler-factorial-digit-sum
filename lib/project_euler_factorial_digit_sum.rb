bc3d02f6f6591078ccde79227ca488b2ddde68a7
github username <github email address>
2021-10-01 11:58:58 -0400

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,19 @@
 # code your solution here
+def factorial(num)
+    product = 1
+
+    while num > 0
+        product *= num
+        num -= 1
+    end
+
+    product
+end
+
+def sum_of_digits(num)
+    num.to_s.split('').map(&:to_i).inject(:+)
+end
+
+def factorial_digit_sum(num)
+    sum_of_digits(factorial(num))
+end
